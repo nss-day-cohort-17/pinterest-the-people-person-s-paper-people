c942eba827d085ec644bbcb4ccb9a35a92038f68
Phillip Lewis <phillip.lewisjr@gmail.com>
2017-01-24 10:28:50 -0600

    remove bower components

--- app/controllers/topics.js
+++ app/controllers/topics.js
@@ -5,4 +5,15 @@ app.controller('TopicCtrl', function($scope, cardFactory) {
   .then((data)=> {
     console.log(data.data)
   })
+
+  let getPin = function (uid, boardid, url, title) {
+
+    let pin = {
+      "uid": uid,
+      "boardid": boardid,
+      "url": url,
+      "title": title
+    }
+  }
+  $http.post('https://ppppproject-afa08.firebaseio.com/.json', JSON.stringify(pin))
 })
