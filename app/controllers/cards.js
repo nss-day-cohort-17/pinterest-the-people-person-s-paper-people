bc891f9b5f23d885b38de480ab43a194432693d0
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-01-24 11:09:45 -0600

    git errors

--- app/controllers/cards.js
+++ app/controllers/cards.js
@@ -1,15 +1,4 @@
-app.config(($routeProvider, $locationProvider)=> {
-    $locationProvider.hashPrefix('');
-    $routeProvider
-    .when('/', {
-      controller: 'CardsCtrl',
-      templateUrl: '/partials/cards.html'
-    })
-    .otherwise({redirectTo: '/' })
-})
-
-
-.controller('CardsCtrl', function() {
+app.controller('CardsCtrl', function() {
   console.log('blaaaah')
 })
 
