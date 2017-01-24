15b79223ee26cfa8d34eba6e0998c8dd1924373e
Michael Watson <m.watson.tn@gmail.com>
2017-01-24 12:19:19 -0600

    fixing branch differences

--- app/controllers/cards.js
+++ app/controllers/cards.js
@@ -1,5 +1,3 @@
-
-
 app.controller('CardsCtrl', function($scope) {
   
 })
--- app/controllers/topics.js
+++ app/controllers/topics.js
@@ -1,21 +1,19 @@
 // console.log('topics')
-
-
 app.controller('TopicCtrl', function($scope, $http, cardFactory) {
   cardFactory.getList()
   .then((data)=> {
     console.log(data.data)
 
   })
-
-  let getPin = function (uid, boardid, url, title) {
-
-    let pin = {
-      "uid": uid,
-      "boardid": boardid,
-      "url": url,
-      "title": title
-    }
-  }
-  $http.post('https://ppppproject-afa08.firebaseio.com/.json', JSON.stringify(pin))
 })
+//   let getPin = function (uid, boardid, url, title) {
+
+//     let pin = {
+//       "uid": uid,
+//       "boardid": boardid,
+//       "url": url,
+//       "title": title
+//     }
+//   }
+//   $http.post('https://ppppproject-afa08.firebaseio.com/.json', JSON.stringify(pin))
+// }
