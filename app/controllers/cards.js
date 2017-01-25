df2d444859c57f58dec83c2b00561c544cfcaacd
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-01-25 13:22:43 -0600

    deleted extra js lines in cards.js

--- app/controllers/cards.js
+++ app/controllers/cards.js
@@ -6,9 +6,3 @@ app.controller('CardsCtrl', function($scope, $http, cardFactory) {
   })
 })
 // added md-icon config
-
-app.config(function($mdIconProvider){
-  $mdIconProvider.fontSet('md', 'material-icons')
-})
-
-// app.controller('CardsCtrl' function())
