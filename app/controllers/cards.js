8f9dbbb4fc917a04e80469f21f6cd9cdcbe302eb
Michael Watson <m.watson.tn@gmail.com>
2017-01-26 11:55:31 -0600

    added create board/btn on home

--- app/controllers/cards.js
+++ app/controllers/cards.js
@@ -1,12 +1,15 @@
-
+// will load boards automatically
 app.controller('CardsCtrl', function($scope, $http, cardFactory) {
-  cardFactory.getList()
-  .then((data)=> {
+ //  cardFactory.getList()
+ //  .then((data)=> {
 
 
-    $scope.topicCard = data.topics;
-    console.log($scope.topics)
+ //    $scope.topicCard = data.topics;
+ //    console.log($scope.topics)
 
-	})
+	// })
+
+	// $scope.getALife = "https://www.buzzfeed.com/rachelwmiller/all-we-do-is-pin?utm_term=.wuZapM9qg#.gvJOPZMgD"
+
 })
 // added md-icon config
