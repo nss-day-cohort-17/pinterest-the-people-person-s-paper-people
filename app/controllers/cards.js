babcad5437cc63fabe184bc493919e716223cd01
Michael Watson <m.watson.tn@gmail.com>
2017-01-26 14:48:49 -0600

    cleaned up homepage btns and link to pinBoards

--- app/controllers/cards.js
+++ app/controllers/cards.js
@@ -1,6 +1,13 @@
 // will load boards automatically
-app.controller('CardsCtrl', function($scope, $http, cardFactory) {
+app.controller('CardsCtrl', function($scope, $http, cardFactory, $mdDialog) {
 	cardFactory.getList()
   .then((data)=>{
-    $scope.topicCard = data.topics;
+    // $scope.topicCard = data.topics;
   })
+   // $scope.textInputShow = false;
+   // $scope.createBoardClick = () => {
+   // 		textInputShow = true
+   // }
+   // $scope.textInput = ""
+   // console.log($scope.textInput)
+})
